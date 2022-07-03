9c437326e7ebd958ad5b0356b07b38ab11102f65
Jessie Chen <notionj.jc@gmail.com>
2022-07-03 17:55:43 -0400

    Prevent reclicking on board and clicking when board is full

--- script.js
+++ script.js
@@ -1,34 +1,35 @@
 const boardDisplay = document.getElementById("board");
 
 const board = [
-    // [0,1,2],
-    // [3,4,5],
-    // [6,7,8]
     [null, null, null],
     [null, null, null],
     [null, null, null]
 ];
 const Player = (marker) => {
     const setMarker = () => marker;
-    const switchPlayers = () => {};
-    return {setMarker, switchPlayers};
+    return {setMarker};
 };
 const player1 = Player("X");
 const player2 = Player("O");
 
-let counter  = 0;
-function playMarker(){
-    while(counter < 10){
-        if (counter % 2 == 0){
-            counter++;
-            return (player2.setMarker());
-        }
-        else if(counter % 2 == 1){
-            counter++;
-            return (player1.setMarker());
+
+const markerSetter = (() =>{
+    let counter  = 0;
+    const count = () => {
+        while(counter < 9){
+            if (counter % 2 == 0){
+                counter++;
+                return (player2.setMarker());
+            }
+            else if(counter % 2 == 1){
+                counter++;
+                return (player1.setMarker());
+            }
         }
     }
-}
+    return {count};
+})();
+
 
 for (let i = 0; i < 3; i++){
     for (let j = 0; j < 3; j++){
@@ -37,7 +38,11 @@ for (let i = 0; i < 3; i++){
         cell.id = `${i},${j}`;
         cell.innerText = board[i][j];
         boardDisplay.append(cell);
-        cell.addEventListener("click", () => {cell.innerHTML = playMarker()});
+        cell.addEventListener("click", () => {
+            if (cell.innerText === ""){
+                cell.innerHTML = markerSetter.count();
+            }
+        });
 
     }
 }
